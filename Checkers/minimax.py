8b14b9b2875f680a3873e0dc0db23384ba0a4949
shykiu77 <carlosgabrielsns@gmail.com>
2019-12-08 00:10:10 -0300

    Implemented minimax tree generation.

---
+++ Checkers/minimax.py
@@ -0,0 +1,60 @@
+import sys
+import copy
+BLACK = 0
+WHITE = 1
+NONE = -1
+MAXDEPTH = 3
+
+class Node:
+    def __init__(self,pattern,parent,children,turn):
+        self.pattern = pattern
+        self.parent = parent
+        self.children = children
+        self.turn = turn
+    
+    
+
+    
+
+def printBoard(board):
+    for i in range(0,8):
+        for j in range(0,8):
+            print("board[" + str(i) + "]["+ str(j) + "] = ",end = '')
+            if board[i][j] == BLACK:
+                print("BLACK")
+            elif board[i][j] == WHITE:
+                print("WHITE")
+            else: 
+                print("None")
+
+def GenerateMoves(board,turn,parent):
+    moves = []
+    for i in range(0,3):
+        moves.append(Node(copy.deepcopy(board),parent,[],turn))
+    return moves
+
+def GenerateTree(node,depth):
+    if depth > MAXDEPTH:
+        return
+    node.children = GenerateMoves(node.pattern,BLACK if depth%2 == 0 else WHITE,node)
+    for element in node.children:
+        GenerateTree(element,depth+1)
+    
+
+def main():
+    board = []
+    for i in range(0,8):
+        column = []
+        for j in range(0,8):
+            if j <= 2 and (i+j)%2 != 0:
+                column.append(WHITE)
+            elif j >= 5 and (i+j)%2 != 0:
+                column.append(BLACK)
+            else:
+                column.append(None)
+        board.append(column)
+    root = Node(board,None,[],WHITE)
+    GenerateTree(root,0)
+    
+if __name__ == '__main__':
+    main()
